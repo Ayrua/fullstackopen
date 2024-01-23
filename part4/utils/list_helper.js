0c6402f9862df098704b38b09ece268024770b35
Alan <20212970+Ayrua@users.noreply.github.com>
2024-01-23 14:02:04 +0100

    4.4 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (array) => {
+  const sum = array.reduce((sum, val) => sum + val.likes, 0)
+  // console.log(sum)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
