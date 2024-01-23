bb53b31576bf0f9c4b383249f4c3b7440c593d0c
Alan <20212970+Ayrua@users.noreply.github.com>
2024-01-23 14:56:58 +0100

    4.7 done

--- part4/tests/list.test.js
+++ part4/tests/list.test.js
@@ -33,43 +33,44 @@ describe('total likes', () => {
 })
 
 describe('favorite Blog', () => {
-
+  // skipping empty array for now
   test('of multiple blog list', () => {
     const mostLiked = {
-      _id: "5a422b3a1b54a676234d17f9",
-      title: "Canonical string reduction",
-      author: "Edsger W. Dijkstra",
-      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-      likes: 12,
-      __v: 0
+      author: "Robert C. Martin",
+      blogs: 3
     }
-    const result = listHelper.favoriteBlog(listData.listWithMultipleBlogs)
+    const result = listHelper.mostBlogs(listData.listWithMultipleBlogs)
     expect(result).toEqual(mostLiked)
   })
 
   test('of single blog list', () => {
     const mostLiked = {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
+      author: "Edsger W. Dijkstra",
+      blogs: 1
     }
-    const result = listHelper.favoriteBlog(listData.listWithOneBlog)
+    const result = listHelper.mostBlogs(listData.listWithOneBlog)
     expect(result).toEqual(mostLiked)
   })
 
-  test('of single blog list with zero Likes', () => {
+})
+
+describe('most likes Blog', () => {
+  // again skipping zero array
+  test('of multiple blog list', () => {
     const mostLiked = {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 0,
-      __v: 0
+      author: "Edsger W. Dijkstra",
+      likes: 17
     }
-    const result = listHelper.favoriteBlog(listData.listWithOneBlogZeroLikes)
+    const result = listHelper.mostLikes(listData.listWithMultipleBlogs)
+    expect(result).toEqual(mostLiked)
+  })
+
+  test('of multiple blog list', () => {
+    const mostLiked = {
+      author: "Edsger W. Dijkstra",
+      likes: 5
+    }
+    const result = listHelper.mostLikes(listData.listWithOneBlog)
     expect(result).toEqual(mostLiked)
   })
 
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -14,6 +16,33 @@ const favoriteBlog = (array) => {
   return favorite
 }
 
+const mostBlogs = (array) => {
+  const names = array.map(x => x.author)
+  const name = (_.head(_(names).countBy().entries().maxBy(_.last)))
+  const value = names.reduce((sum, val) => val === name ? sum + 1 : sum + 0, 0)
+  // console.log(value)
+  return ({ "author": name, "blogs": value })
+}
+
+const mostLikes = (array) => {
+  const authors = array.map(x => x.author)
+  const names = [...new Set(authors)]
+  const mapped = names.map(x => ({ "author": x, "likes": 0 }))
+  let maxAuthor = { "author": '', "likes": 0 }
+  mapped.forEach((x, i) => {
+    let name = x.author
+    let likes = x.likes
+    //console.log(name, likes)
+    const sum = array.reduce((sum, val) => val.author === name ? sum + val.likes : sum + 0, 0)
+    //console.log(name, sum)
+    if (sum > maxAuthor.likes) {
+      maxAuthor.author = name
+      maxAuthor.likes = sum
+    }
+  })
+  return maxAuthor
+}
+
 module.exports = {
-  dummy, totalLikes, favoriteBlog
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
