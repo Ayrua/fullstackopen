1c83c714070b54b86624a11e06295bbb244b21ca
Alan <20212970+Ayrua@users.noreply.github.com>
2024-01-25 15:44:06 +0100

    4.13 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,14 +3,19 @@ const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-  return response.json(blogs)
+  response.json(blogs).end()
 })
 
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
   const result = await blog.save()
-  return response.status(201).json(result)
+  response.status(201).json(result).end()
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -16,71 +16,93 @@ beforeEach(async () => {
   }
 })
 
+describe('get requests tests', () => {
+  test('correct length of blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(6)
+  })
 
-test('correct length of blogs', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(6)
+  test('the unique identifier is named id', async () => {
+    const response = await api.get('/api/blogs')
+    const firstBlog = response.body[0]
+    // console.log(firstBlog)
+    expect(firstBlog['id']).toBeDefined();
+  });
 })
 
-test('the unique identifier is named id', async () => {
-  const response = await api.get('/api/blogs')
-  const firstBlog = response.body[0]
-  // console.log(firstBlog)
-  expect(firstBlog['id']).toBeDefined();
-});
+describe('post request tests', () => {
+  test('check if blogs increase by one if added', async () => {
+    const testBlog = {
+      'title': 'test title',
+      'author': 'test name',
+      'url': 'test url',
+      'likes': 0
+    }
+    const firstResponse = await api.get('/api/blogs')
+    const firstLength = firstResponse.body.length
 
-test('check if blogs increase by one if added', async () => {
-  const testBlog = new Blog({
-    'title': 'test title',
-    'author': 'test name',
-    'url': 'test url',
-    'likes': 0
+    const postResponse = await api.post('/api/blogs').send(testBlog)
+    const postID = postResponse.body['id']
+
+    const secondResponse = await api.get('/api/blogs')
+    expect(secondResponse.body).toHaveLength(firstLength + 1)
+
+    // check if id in database
+    const thirdResponse = await api.get('/api/blogs')
+    const checker = thirdResponse.body.some(x => x.id === postID)
+    expect(checker).toBe(true)
   })
-  const firstResponse = await api.get('/api/blogs')
-  const firstLength = firstResponse.body.length
-
-  const postResponse = await api.post('/api/blogs', testBlog)
-  const postID = postResponse.body['id']
-
-  const secondResponse = await api.get('/api/blogs')
-  expect(secondResponse.body).toHaveLength(firstLength + 1)
-
-  // check if id in database
-  const thirdResponse = await api.get('/api/blogs')
-  const checker = thirdResponse.body.some(x => x.id === postID)
-  expect(checker).toBe(true)
 })
 
-test('check if blogs created without likes, have 0', async () => {
-  const testBlog = new Blog({
-    'title': 'test title 2',
-    'author': 'test name 2',
-    'url': 'test url 2',
-  })
-  const postResponse = await api.post('/api/blogs', testBlog)
-  const postID = postResponse.body['id']
+describe('schema tests', () => {
+  test('check if blogs created without likes, have 0', async () => {
+    const testBlog = {
+      'title': 'test title 2',
+      'author': 'test name 2',
+      'url': 'test url 2',
+    }
+    const postResponse = await api.post('/api/blogs').send(testBlog)
+    const postID = postResponse.body['id']
 
-  const getResponse = await api.get('/api/blogs')
-  const blogObject = getResponse.body.find(x => x.id === postID)
-  expect(blogObject.likes).toBe(0)
+    const getResponse = await api.get('/api/blogs')
+    const blogObject = getResponse.body.find(x => x.id === postID)
+    // console.log(blogObject)
+    expect(blogObject.likes).toBe(0)
+  })
+
+  test('check if blogs created without title return 400', async () => {
+    const testBlog = {
+      // 'title': 'test title 3',
+      'author': 'test name 3',
+      'url': 'test url 3',
+    }
+    await api.post('/api/blogs').send(testBlog).expect(400)
+  })
+
+  test('check if blogs created without url return 400', async () => {
+    const testBlog = {
+      'title': 'test title 4',
+      'author': 'test name 4',
+      // 'url': 'test url 4',
+    }
+    await api.post('/api/blogs').send(testBlog).expect(400)
+  })
 })
 
-test('check if blogs created without title return 400', async () => {
-  const testBlog = new Blog({
-    // 'title': 'test title 3',
-    'author': 'test name 3',
-    'url': 'test url 3',
-  })
-  await api.post('/api/blogs', testBlog).expect(400)
-})
+describe('delete request test', () => {
+  test('check if blogs are deleted by id', async () => {
+    const testBlog = {
+      'title': 'test title 5',
+      'author': 'test name 5',
+      'url': 'test url 5',
+    }
+    const response = await api.post('/api/blogs').send(testBlog)
+    const postID = response.body['id']
+    //console.log(response.body)
+    //console.log(postID)
 
-test('check if blogs created without url return 400', async () => {
-  const testBlog = new Blog({
-    'title': 'test title 4',
-    'author': 'test name 4',
-    // 'url': 'test url 4',
+    await api.delete(`/api/blogs/${postID}`).expect(204)
   })
-  await api.post('/api/blogs', testBlog).expect(400)
 })
 
 afterAll(async () => {
