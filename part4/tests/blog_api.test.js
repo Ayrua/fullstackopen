a5fdb494c8062ec9c5ac36f4f26afd89185c1d64
Alan <20212970+Ayrua@users.noreply.github.com>
2024-01-25 14:30:03 +0100

    4.10 done

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -29,6 +29,28 @@ test('the unique identifier is named id', async () => {
   expect(firstBlog['id']).toBeDefined();
 });
 
+test('check if blogs increase by one if added', async () => {
+  const testBlog = new Blog({
+    'title': 'test title',
+    'author': 'test name',
+    'url': 'test url',
+    'likes': 0
+  })
+  const firstResponse = await api.get('/api/blogs')
+  const firstLength = firstResponse.body.length
+
+  const postResponse = await api.post('/api/blogs', testBlog)
+  const postID = postResponse.body['id']
+
+  const secondResponse = await api.get('/api/blogs')
+  expect(secondResponse.body).toHaveLength(firstLength + 1)
+
+  // check if id in database
+  const thirdResponse = await api.get('/api/blogs')
+  const checker = thirdResponse.body.some(x => x.id === postID)
+  expect(checker).toBe(true)
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
