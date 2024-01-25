1128ad0c24b9c1dc3ba2c3580f02bcd3764591ad
Alan <20212970+Ayrua@users.noreply.github.com>
2024-01-25 17:56:18 +0100

    4.20 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,15 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const jwt = require('jsonwebtoken')
-
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -19,10 +10,7 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
+  const decodedToken = request.token
 
   const user = await User.findById(body.userId)
   const blog = new Blog({
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,4 +1,5 @@
 const logger = require('./logger')
+const jwt = require('jsonwebtoken')
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
@@ -11,7 +12,25 @@ const errorHandler = (error, request, response, next) => {
 
   next(error)
 }
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+const tokenExtractor = (request, response, next) => {
+  if (getTokenFrom(request).includes('Bearer')) {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    request.token = decodedToken
+  }
+  next()
+}
 
 module.exports = {
-  errorHandler
+  errorHandler, tokenExtractor
 }
