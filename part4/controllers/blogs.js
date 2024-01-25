80b55572dc03c3a46053b45747663609c753c09b
Alan <20212970+Ayrua@users.noreply.github.com>
2024-01-25 17:49:12 +0100

    4.19 done

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,6 +18,12 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
   const user = await User.findById(body.userId)
   const blog = new Blog({
     'title': body.title,
@@ -17,7 +32,6 @@ blogsRouter.post('/', async (request, response) => {
     'likes': body.likes,
     'user': user.id
   })
-
 
   const result = await blog.save()
 
