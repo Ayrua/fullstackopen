f892e5941a6752f2604eb9ae49175913ee0a1ba4
Alan <20212970+Ayrua@users.noreply.github.com>
2024-01-25 17:39:20 +0100

    4.15-4.19 done

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,8 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const PORT = config.PORT
 const url = config.MONGODB_URI
@@ -22,6 +24,8 @@ mongoose.connect(url)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.errorHandler)
 
 
---
+++ part4/tests/user.test.js
@@ -0,0 +1,135 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const bcrypt = require('bcrypt')
+const User = require('../models/user')
+const helper = require('../utils/helper')
+
+
+beforeEach(async () => {
+  await User.deleteMany({})
+
+  const passwordHash = await bcrypt.hash('sekret', 10)
+  const user = new User({ username: 'root', passwordHash })
+
+  await user.save()
+})
+
+describe('when there is initially one user in db', () => {
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('expected `username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toEqual(usersAtStart)
+  })
+})
+
+describe('when there is a validation error check for correct codes', () => {
+  test('get error 400 when usename too short', async () => {
+    const newUser = {
+      "username": "ro",
+      "name": "test",
+      "password": "12345678"
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('is shorter than the minimum allowed length (3)')
+  })
+
+  test('get error 400 when password is too short', async () => {
+    const newUser = {
+      "username": "user123",
+      "name": "test",
+      "password": "12"
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('password needs to be at least 3 chars long')
+  })
+})
+
+describe('when there is a part not given', () => {
+  test('get error 400 when usename is missing', async () => {
+    const newUser = {
+      // "username": "roo",
+      "name": "test",
+      "password": "12345678"
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('username: Path `username` is required.')
+  })
+
+  test('get error 400 when password is not given', async () => {
+    const newUser = {
+      "username": "testuser",
+      "name": "test"
+      //"password": "12"
+    }
+    console.log(newUser)
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('need a password')
+  })
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
