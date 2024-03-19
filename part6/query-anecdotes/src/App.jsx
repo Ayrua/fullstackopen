f109d9c55097a94d7d96ed6119e22ba5d5249012
Alan <20212970+Ayrua@users.noreply.github.com>
2024-03-19 15:31:10 +0100

    6.20-6.22 done

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,40 +1,59 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import { getAnecdotes, updateAnecdote } from "./services/requests";
 
 const App = () => {
+  const queryClient = useQueryClient();
+  const newAnecdoteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      const anecdotes = queryClient.getQueryData(["anecdotes"]);
+      const updatedAnecArr = anecdotes.map((a) =>
+        a.id == updatedAnecdote.id ? updatedAnecdote : a
+      );
+      queryClient.setQueryData(["anecdotes"], updatedAnecArr);
+    },
+  });
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    console.log("vote");
+    const updatedAnecdote = { ...anecdote, votes: anecdote.votes + 1 };
+    newAnecdoteMutation.mutate(updatedAnecdote);
+  };
+
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    retry: 1,
+    refetchOnWindowFocus: false,
+  });
+  console.log(JSON.parse(JSON.stringify(result)));
+
+  if (result.isLoading) {
+    return <div>anecdote service not available due to problems on server</div>;
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const anecdotes = result.data;
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
